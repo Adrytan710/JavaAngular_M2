df4d080162318ce3050399e61d54369c5eb9d4d0
JoanR17 <joanr.m.1999@gmail.com>
2022-01-24 09:26:54 +0100

    Commit Mil2

--- Java_M2_Jobs/src/Main/MainApp.java
+++ Java_M2_Jobs/src/Main/MainApp.java
@@ -6,17 +6,23 @@ public class MainApp {
 	public static void main(String[] args) {
 		Trabajadores t1 = new Trabajadores(Empleados.Boss,1000);
 		Trabajadores t2 = new Trabajadores(Empleados.Manager,1100);
-		Trabajadores t3 = new Trabajadores(Empleados.Employee,1200);
-		Trabajadores t4 = new Trabajadores(Empleados.Volunteer,1300);
+		Trabajadores t3 = new Trabajadores(Empleados.Senior,1200);
+		Trabajadores t4 = new Trabajadores(Empleados.Mid,1200);
+		Trabajadores t5 = new Trabajadores(Empleados.Junior,1200);
+		Trabajadores t6 = new Trabajadores(Empleados.Volunteer,1300);
 		
 		Salario.calcularSalario(t1);
 		Salario.calcularSalario(t2);
 		Salario.calcularSalario(t3);
 		Salario.calcularSalario(t4);
+		Salario.calcularSalario(t5);
+		Salario.calcularSalario(t6);
 		
 		System.out.println(t1);
 		System.out.println(t2);
 		System.out.println(t3);
 		System.out.println(t4);
+		System.out.println(t5);
+		System.out.println(t6);
 	}
 }
--- Java_M2_Jobs/src/Objetos/Trabajadores.java
+++ Java_M2_Jobs/src/Objetos/Trabajadores.java
@@ -11,63 +11,64 @@ public class Trabajadores {
 
 	private double comprobarSalario(double salario, Empleados empleado)
 	{
+		double sal = salario;
 		switch(empleado) 
 		{
 			case Boss:
-				if(salario < 8000)
+				if(sal < 8000)
 				{
-					salario = 8000;
+					sal = 8000;
 				}
 				break;
 			case Manager:
-				if(salario < 3000)
+				if(sal < 3000)
 				{
-					salario = 3000;
+					sal = 3000;
 				}
-				else if(salario > 5000)
+				else if(sal > 5000)
 				{
-					salario = 5000;
+					sal = 5000;
 				}
 				break;
 			case Senior:
-				if(salario < 2700)
+				if(sal < 2700)
 				{
-					salario = 2700;
+					sal = 2700;
 				}
-				else if(salario > 4000)
+				else if(sal > 4000)
 				{
-					salario = 4000;
+					sal = 4000;
 				}
 				break;
 			case Mid:
-				if(salario < 1800)
+				if(sal < 1800)
 				{
-					salario = 1800;
+					sal = 1800;
 				}
-				else if(salario > 2500)
+				else if(sal > 2500)
 				{
-					salario = 2500;
+					sal = 2500;
 				}
 				break;
 			case Junior:
-				if(salario < 900)
+				if(sal < 900)
 				{
-					salario = 900;
+					sal = 900;
 				}
-				else if(salario > 1600)
+				else if(sal > 1600)
 				{
-					salario = 1600;
+					sal = 1600;
 				}
 				break;
 			case Volunteer:
-				if(salario != 0)
+				if(sal != 0)
 				{
-					salario = 0;
+					sal = 0;
 				}
 				break;
 		}
 
-		return salario;
+		return sal;
 	}
 
 	@Override
